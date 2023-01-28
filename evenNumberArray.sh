799f9a409560e2dcc4d800a0c37c16eda76a4e51
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-28 12:27:24 +0530

    Add: Workshop problem Updated

--- evenNumberArray.sh
+++ evenNumberArray.sh
@@ -3,7 +3,11 @@ while [ $counter -le 50 ]
 do
         if [ $(($counter % 2)) == 0 ]
         then
-			echo "Even Number is $counter" 
+
+			echo "Even Number is $counter"
+			Even=($counter)
+			echo "Array values "${Even[@]}
         fi
+
 ((counter++))
 done
